1037625cd06e1f973494f311fd8dbe31ecadc6c4
Michael APPLIX <michaelapplix@gmail.com>
2025-09-28 13:45:35 +0000

    when creating a account it says auth/configuration-no-found

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: "studio-3238744839-3220a",
-  appId: "1:643251024593:web:f20b75e2ee70f7171d8bf0",
-  apiKey: "AIzaSyDsNpsPcXI0CN1xeBjWrmRLfBlUdaLGl1w",
-  authDomain: "studio-3238744839-3220a.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "643251024593"
+  projectId: 'studio-3238744839-3220a',
+  appId: '1:643251024593:web:f20b75e2ee70f7171d8bf0',
+  apiKey: 'AIzaSyDsNpsPcXI0CN1xeBjWrmRLfBlUdaLGl1w',
+  authDomain: 'studio-3238744839-3220a.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '643251024593',
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
